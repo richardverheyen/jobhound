a52b14806aacae5c02a1d16e3d7d646b33f99536
Richard <richard.verheyen@gmail.com>
2025-03-23 16:15:58 +1100

    fix middleware

--- supabase/middleware.ts
+++ supabase/middleware.ts
@@ -62,7 +62,7 @@ export async function updateSession(request: NextRequest) {
   //    return myNewResponse
   // If this is not done, you may be causing the browser and server to go out
   // of sync and terminate the user's session prematurely!
-  return supabaseResponse
+  return response
 }
 
 /**
