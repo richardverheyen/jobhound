cf1f64de9bd1278de01f609b55f10a52e8c38be6
Richard <richard.verheyen@gmail.com>
2025-04-20 15:16:53 +1000

    Fix the middleware to show our favicon

--- middleware.ts
+++ middleware.ts
@@ -9,14 +9,4 @@ export async function middleware(request: NextRequest) {
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * - public folder
-     */
-    '/((?!_next/static|_next/image|favicon.ico|public).*)',
-  ],
 };
